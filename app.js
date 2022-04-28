d172da1fd30dafb5877ec9f0d5ce3d1e7a24f188
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-04-28 21:11:42 +0200

    feat : header and main section

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
+const mainSection = document.querySelector(".main-section")
+
+
 const fetchData = () => {
     fetch("https://themealdb.com/api/json/v1/1/categories.php").then(res => {
         return res.json()
     }).then(data => {
         console.log(data);
+        displayMeals(data)
     })
 }
 
 fetchData()
+
+
+const displayMeals = mealsCards => {
+    mealsCards.forEach(card => {
+        
+
+        
+    });
+}
